7e6c81fdc00726c9d51b25c52d7e846f74f21cb9
천진아 <totter15@naver.com>
2022-04-15 16:38:21 +0900

    feat:MainTab Camera madal

---
+++ compoenets/CameraButton.js
@@ -0,0 +1,89 @@
+import React, {useState} from 'react';
+import {View, Pressable, StyleSheet, Platform} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import UploadModeModal from './UploadModeModal';
+import {launchImageLibrary, launchCamera} from 'react-native-image-picker';
+
+const TABBAR_HEIGHT = 49;
+
+const imagePickerOption = {
+  mediaType: 'photo',
+  maxWidth: 768,
+  maxHeight: 768,
+  includeBase64: Platform.OS === 'android',
+};
+
+const CameraButton = () => {
+  const insets = useSafeAreaInsets();
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const bottom = Platform.select({
+    android: TABBAR_HEIGHT / 2,
+    ios: TABBAR_HEIGHT / 2 + insets.bottom - 4,
+  });
+
+  const onPickImage = res => {
+    if (res.didCancel || !res) {
+      return;
+    }
+    console.log(res);
+  };
+  const onLaunchCamera = () => {
+    launchCamera(imagePickerOption, onPickImage);
+  };
+
+  const onLaunchImageLibrary = () => {
+    launchImageLibrary(imagePickerOption, onPickImage);
+  };
+
+  return (
+    <View style={[styles.wrapper, {bottom}]}>
+      <Pressable
+        android_ripple={{color: '#ffffff'}}
+        style={styles.circle}
+        onPress={() => setModalVisible(true)}>
+        <Icon name="camera-alt" color="white" size={24} />
+      </Pressable>
+      <UploadModeModal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+        onLaunchCamera={onLaunchCamera}
+        onLaunchImageLibrary={onLaunchImageLibrary}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  wrapper: {
+    zIndex: 5,
+    borderRadius: 27,
+    height: 54,
+    width: 54,
+    position: 'absolute',
+    left: '50%',
+    transform: [{translateX: -27}],
+    ...Platform.select({
+      ios: {
+        shadowColor: '#4d4d4d',
+        shadowOffset: {width: 0, height: 4},
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+        overflow: 'hidden',
+      },
+    }),
+  },
+  circle: {
+    backgroundColor: '#6200ee',
+    borderRadius: 27,
+    height: 54,
+    width: 54,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default CameraButton;
