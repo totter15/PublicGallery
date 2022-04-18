8ea2e4ff14c7fdeed11d0b7073141e66232a8d12
천진아 <totter15@naver.com>
2022-04-18 15:20:20 +0900

    feat:RefreshPost, PostPagenation

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,20 +1,64 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList} from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  RefreshControl,
+} from 'react-native';
 import PostCard from '../compoenets/PostCard';
-import {getPosts} from '../lib/posts';
+import {getNewerPosts, getOlderPosts, getPosts, PAGE_SIZE} from '../lib/posts';
 
 const FeedScreen = () => {
   const [posts, setPosts] = useState(null);
+  const [noMorePost, setNoMorePost] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
 
   useEffect(() => {
     getPosts().then(setPosts);
   }, []);
+
+  const onLoadMore = async () => {
+    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
+      return;
+    }
+    const lastPost = posts[posts.length - 1];
+    const olderPosts = await getOlderPosts(lastPost.id);
+    if (getOlderPosts.length < PAGE_SIZE) {
+      setNoMorePost(true);
+    }
+    setPosts(posts.concat(olderPosts));
+  };
+
+  const onRefresh = async () => {
+    if (!posts || posts.length === 0 || refreshing) {
+      return;
+    }
+    const firstpost = posts[0];
+    setRefreshing(true);
+    const newerPosts = await getNewerPosts(firstpost.id);
+    setRefreshing(false);
+    if (newerPosts.length === 0) {
+      return;
+    }
+    setPosts(newerPosts.concat(posts));
+  };
 
   return (
     <FlatList
       data={posts}
       renderItem={renderItem}
       keyExtractor={item => item.id}
+      contentContainerStyle={styles.container}
+      onEndReached={onLoadMore}
+      onEndReachedThreshold={0.75}
+      ListFooterComponent={
+        !noMorePost && (
+          <ActivityIndicator style={styles.spinner} size={32} color="#6200ee" />
+        )
+      }
+      refreshControl={
+        <RefreshControl onRefresh={onRefresh} refreshing={refreshing} />
+      }
     />
   );
 };
@@ -28,5 +72,14 @@ const renderItem = ({item}) => (
     photoURL={item.photoURL}
   />
 );
+
+const styles = StyleSheet.create({
+  container: {
+    paddingBottom: 48,
+  },
+  spinner: {
+    height: 64,
+  },
+});
 
 export default FeedScreen;
