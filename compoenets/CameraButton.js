026f22f5d0ea01bff1f8d5365dc369d8a9ac4367
천진아 <totter15@naver.com>
2022-04-27 14:55:41 +0900

    refactor:ActionSheetModal로 재사용가능 모달 작성

--- compoenets/CameraButton.js
+++ compoenets/CameraButton.js
@@ -2,9 +2,9 @@ import React, {useState} from 'react';
 import {View, Pressable, StyleSheet, Platform} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import UploadModeModal from './UploadModeModal';
 import {launchImageLibrary, launchCamera} from 'react-native-image-picker';
 import {useNavigation} from '@react-navigation/native';
+import ActionSheetModal from './ActionSheetModal';
 
 const TABBAR_HEIGHT = 49;
 
@@ -47,11 +47,21 @@ const CameraButton = () => {
         onPress={() => setModalVisible(true)}>
         <Icon name="camera-alt" color="white" size={24} />
       </Pressable>
-      <UploadModeModal
+      <ActionSheetModal
         visible={modalVisible}
         onClose={() => setModalVisible(false)}
-        onLaunchCamera={onLaunchCamera}
-        onLaunchImageLibrary={onLaunchImageLibrary}
+        actions={[
+          {
+            icon: 'camera-alt',
+            text: '카메라로 촬영하기',
+            onPress: onLaunchCamera,
+          },
+          {
+            icon: 'photo',
+            text: '사진 선택하기',
+            onPress: onLaunchImageLibrary,
+          },
+        ]}
       />
     </View>
   );
